4001982dd6a56a544d68d25cf30b1e559e737fe5
RickyAnchores <ricky23anchores@gmail.com>
2023-02-04 19:10:56 +0000

    added grid

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='Home'>
-        <h1>Home</h1>
+      <div className="grid">
+        <div className="A">
+          <h1>Home</h1>
+          <p>This is Home</p>
+        </div>
+        <div className="B">
+          <img src="https://images.unsplash.com/photo-1476357471311-43c0db9fb2b4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
