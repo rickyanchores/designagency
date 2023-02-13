1d1de079d85b1280bdb0f4987bcdafe3dd8c69e1
RickyAnchores <ricky23anchores@gmail.com>
2023-02-13 17:44:03 +0000

    added cards

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,6 +5,23 @@ const About = () => {
     <div className='About'>
         <h3>About</h3>
         <p>this is about</p>
+        <div className="team">
+          <div className="user">
+            <img src="" alt="" />
+            <h2>Vash the Stampede</h2>
+            <h3>Front End Dev</h3>
+          </div>
+          <div className="user">
+            <img src="" alt="" />
+            <h2>Monket D Luffy</h2>
+            <h3>Back End Dev</h3>
+          </div>
+          <div className="user">
+            <img src="" alt="" />
+            <h2>Naruto Uzumaki</h2>
+            <h3>Dev OPS</h3>
+          </div>
+        </div>
     </div>
   )
 }
